f8763ea37e787c4116280ec424e06899f6f115d0
Sheikh Jabed <jabedbd2295@gmail.com>
2024-10-16 15:32:29 +0600

    Todo JSX File Updated

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -6,7 +6,7 @@
 // }
 
 
-// Conditional Rendering - Option: 1
+// Conditional Rendering - Option: 1 (if, else conditions)
 // export default function Todo({task, isDone}) {
 //     if (isDone === true) {
 //         return <li>Finished: {task}</li>
@@ -21,10 +21,47 @@
 // }
 
 
-// Conditional Rendering - Option: 2
+// Conditional Rendering - Option: 2 (only if condition)
+// export default function Todo({task, isDone}) {
+//     if (isDone) {
+//         return <li>Finished: {task}</li>
+//     }
+//     return <li>Work on: {task}</li>
+// }
+
+
+// Conditional Rendering - Option: 3 (Ternary)
+// export default function Todo({task, isDone}) {
+//     return (
+//         <li>{isDone ? "Finished" : "Work on"} : {task}</li>
+//     )   
+// }
+
+
+// Conditional Rendering - Option: 4 (&&)
+// export default function Todo({task, isDone}) {
+//     return (
+//         <li>{task} {isDone && ":Done"}</li>
+//     )   
+// }
+
+
+// Conditional Rendering - Option: 5 (||)
+// export default function Todo({task, isDone}) {
+//     return (
+//         <li>{task} {isDone || ":Do It"}</li>
+//     )   
+// }
+
+
+// Conditional Rendering - Option: 6 (using variable)
 export default function Todo({task, isDone}) {
+    let listItem;
     if (isDone) {
-        return <li>Finished: {task}</li>
+        listItem = <li>Finished: {task}</li>
     }
-    return <li>Work on: {task}</li>
+    else {
+        listItem = <li>Work On: {task}</li> 
+    }
+    return listItem;
 }
